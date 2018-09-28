56b440b67fcccde45a83ad4d644a44215333dcc9
ZhangNingNina <42830290+ZhangNingNina@users.noreply.github.com>
2018-09-28 18:23:42 +0800

    Add files via upload

---
+++ 0928ex2.py
@@ -0,0 +1,5 @@
+headers = ['Month', 'Instalment', 'Interest', 'Principal', 'Outstanding']
+for i in range(5):
+    #print(i, 'th', 'header') i means 1,2,3,4,5 so it's not true
+    print(headers[i],'\t',end='')
+    #print(headers[i],'\t',end='\t')
